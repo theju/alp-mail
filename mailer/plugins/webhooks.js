1056ba2515e826e446896a795d2920e8d3e2d454
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2017-07-14 18:51:53 +0530

    Indentation fixes.

--- mailer/plugins/webhooks.js
+++ mailer/plugins/webhooks.js
@@ -8,38 +8,38 @@ exports.hook_rcpt = function(next, connection, params) {
     var cfg = plugin.config.get('webhooks.ini');
     connection.notes.config = cfg;
     request.post(cfg.main.auth_url)
-	.form({
-	    "recipients": transaction.rcpt_to[0].user + "@" + transaction.rcpt_to[0].host,
-	    "sender": transaction.mail_from.original
-	})
-	.on('response', function(response) {
-	    response.on('data', function(data) {
-	    	if (response.statusCode >= 200 && response.statusCode < 300) {
-	    	    next(OK, data);
-	    	}
-	    	if (response.statusCode >= 400 && response.statusCode < 500) {
-	    	    next(DENY, data);
-	    	}
-	    	if (response.statusCode >= 500) {
-	    	    next(DENYSOFT, data);
-	    	}
-	    });
-	})
-	.on('error', function(error, response, body) {
-	    next(DENYSOFT, body);
-	});
+	      .form({
+	          "recipients": transaction.rcpt_to[0].user + "@" + transaction.rcpt_to[0].host,
+	          "sender": transaction.mail_from.original
+	      })
+	      .on('response', function(response) {
+	          response.on('data', function(data) {
+	    	        if (response.statusCode >= 200 && response.statusCode < 300) {
+	    	            next(OK, data);
+	    	        }
+	    	        if (response.statusCode >= 400 && response.statusCode < 500) {
+	    	            next(DENY, data);
+	    	        }
+	    	        if (response.statusCode >= 500) {
+	    	            next(DENYSOFT, data);
+	    	        }
+	          });
+	      })
+	      .on('error', function(error, response, body) {
+	          next(DENYSOFT, body);
+	      });
 }
 
 function getBody(children) {
     var ret = null;
     for (var ii=0; ii < children.length; ii++) {
-	if (children[ii].state === 'body') {
-	    ret = children[ii];
-	} else {
-	    if (children[ii].children.length > 0) {
-		ret = getMultipartAlternative(children[ii].children);
-	    }
-	}
+	      if (children[ii].state === 'body') {
+	          ret = children[ii];
+	      } else {
+	          if (children[ii].children.length > 0) {
+		            ret = getMultipartAlternative(children[ii].children);
+	          }
+	      }
     }
     return ret;
 }
@@ -52,32 +52,32 @@ exports.hook_data_post = function(next, connection, params) {
     var cfg = connection.notes.config;
     var body;
     if (transaction.body.children.length > 0) {
-	body = getBody(transaction.body.children);
+	      body = getBody(transaction.body.children);
     } else {
-	body = transaction.body;
+	      body = transaction.body;
     }
     request.post(cfg.main.mail_url)
-	.form({
-	    "recipients": transaction.rcpt_to[0].user + "@" + transaction.rcpt_to[0].host,
-	    "sender": transaction.mail_from.original,
-	    "subject": transaction.body.header.get("Subject"),
-	    "body": body.bodytext || 'Blank Message',
-	    "headers": transaction.body.header.toString()
-	})
-	.on('response', function(response) {
-	    response.on('data', function(data) {
-	    	if (response.statusCode >= 200 && response.statusCode < 300) {
-	    	    next(OK, data);
-	    	}
-	    	if (response.statusCode >= 400 && response.statusCode < 500) {
-	    	    next(DENY, data);
-	    	}
-	    	if (response.statusCode >= 500) {
-	    	    next(DENYSOFT, data);
-	    	}
-	    });
-	})
-	.on('error', function(error, response, body) {
-	    next(DENYSOFT, "sys_unspecified");
-	});
+	      .form({
+	          "recipients": transaction.rcpt_to[0].user + "@" + transaction.rcpt_to[0].host,
+	          "sender": transaction.mail_from.original,
+	          "subject": transaction.body.header.get("Subject"),
+	          "body": body.bodytext || 'Blank Message',
+	          "headers": transaction.body.header.toString()
+	      })
+	      .on('response', function(response) {
+	          response.on('data', function(data) {
+	    	        if (response.statusCode >= 200 && response.statusCode < 300) {
+	    	            next(OK, data);
+	    	        }
+	    	        if (response.statusCode >= 400 && response.statusCode < 500) {
+	    	            next(DENY, data);
+	    	        }
+	    	        if (response.statusCode >= 500) {
+	    	            next(DENYSOFT, data);
+	    	        }
+	          });
+	      })
+	      .on('error', function(error, response, body) {
+	          next(DENYSOFT, "sys_unspecified");
+	      });
 }
