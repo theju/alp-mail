2b58fecd36affb706d767738ae53c21d6f4202a6
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2017-07-14 18:52:05 +0530

    Handle UTF-8 subjects.

--- server/core/forms.py
+++ server/core/forms.py
@@ -1,3 +1,7 @@
+import re
+import base64
+import quopri
+
 from django import forms
 
 from .models import IncomingEmail
@@ -15,6 +19,18 @@ class EmailFormMeta(object):
 class IncomingEmailForm(EmailForm):
     class Meta(EmailFormMeta):
         model = IncomingEmail
+
+    def clean_subject(self):
+        subject = self.cleaned_data["subject"]
+        utf_subject = re.search("\?utf-8\?(B|Q)\?(.*?)$", subject, re.I)
+        if utf_subject:
+            if utf_subject.groups()[0].lower() == "b":
+                # Base-64 encoded emails
+                subject = base64.b64decode(utf_subject.groups()[1]).decode()
+            else:
+                # Quote-Printable emails
+                subject = quopri.decodestring(utf_subject.groups()[1]).decode()
+        return subject
 
 
 class AuthEmailForm(forms.ModelForm):
