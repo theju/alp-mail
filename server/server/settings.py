0a797f80d783497506e6bbb018197a0cea299a66
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2018-07-08 16:57:53 +0530

    Small change for django2.

--- server/server/settings.py
+++ server/server/settings.py
@@ -41,15 +41,14 @@ INSTALLED_APPS = [
     'core',
 ]
 
-MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
